bfbdc07b62a568872c89eb980df26cad190d40cb
lukeyzhong <lukeyzhong@gmail.com>
2022-09-24 14:37:03 -0500

    update

---
+++ directives/src/app/customdirectives/customdirectives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customdirectives',
+  templateUrl: './customdirectives.component.html',
+  styleUrls: ['./customdirectives.component.css']
+})
+export class CustomdirectivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
