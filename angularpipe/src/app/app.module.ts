8a8d240e5671410596d957988a9ede99a544ccfc
lukeyzhong <lukeyzhong@gmail.com>
2022-10-14 13:50:17 -0500

    update

--- angularpipe/src/app/app.module.ts
+++ angularpipe/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesComponent } from './pipes/pipes.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CustompipeComponent } from './custompipe/custompipe.component';
+import { ConvertPipe } from './custompipe/convert.pipe';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesComponent,
+    CustompipeComponent,
+    ConvertPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
